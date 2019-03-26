8b0537a8ce2bd0e7de634cfa1ee11ab686f2c5c3
MD Servidores Cloud <34929556+servidorescloud@users.noreply.github.com>
2019-03-26 10:00:20 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]> <html class="no-js ie6" lang="pt-BR" prefix="og: http://ogp.me/ns#"> <![endif]-->
+<!--[if IE 7 ]>    <html class="no-js ie7" lang="pt-BR" prefix="og: http://ogp.me/ns#"> <![endif]-->
+<!--[if IE 8 ]>    <html class="no-js ie8" lang="pt-BR" prefix="og: http://ogp.me/ns#"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--> <html class="no-js" lang="pt-BR" prefix="og: http://ogp.me/ns#"> <!--<![endif]-->
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta name="distribution" content="global" />
+<meta name="robots" content="follow, all" />
+
+<script type="text/javascript" src="https://code.jquery.com/jquery-git2.min.js"></script>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<title>NBRP2P</title>
+<style type="text/css">
+.nbrp2p-menu{
+    	margin: 5px;
+        padding: 10px;
+        font-size:15px;
+        font-weight:bold;
+    }
+  
+.navbar-custom {
+    background-color:#135A71;
+    color:#ffffff;
+    border-radius:0;
+}
+
+.navbar-custom .navbar-nav > li > a {
+    color:#fff;
+}
+
+.navbar-custom .navbar-nav > .active > a {
+    color: #ffffff;
+    background-color:transparent;
+}
+
+.navbar-custom .navbar-nav > li > a:hover,
+.navbar-custom .navbar-nav > li > a:focus,
+.navbar-custom .navbar-nav > .active > a:hover,
+.navbar-custom .navbar-nav > .active > a:focus,
+.navbar-custom .navbar-nav > .open >a {
+    text-decoration: none;
+    background-color: #FFAB32;
+}
+
+.navbar-custom .navbar-brand {
+    color:#eeeeee;
+}
+.navbar-custom .navbar-toggle {
+    background-color:#eeeeee;
+}
+.navbar-custom .icon-bar {
+    background-color:#FFAB32;
+}
+
+</style>
+
+</head>
+
+<body>
+<header id="top">
+
+<div class="nbrp2p-menu">
+<nav role="navigation" class="navbar navbar-custom navbar-fixed-top">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        
+        <div class="navbar-header">
+            
+            
+            <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+<a class="navbar-brand" href="">NBRP2P</a>
+
+         </div>
+        <!-- Collection of nav links and other content for toggling -->
+        <div id="navbarCollapse" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+              
+              
+              
+   
+
+<li class="dropdown"><a href="">Inicio</a></li>
+        
+<li class="dropdown"><a href="taxas.php">Taxas</a></li>
+        
+
+        
+            </ul>
+            
+            
+        </div>
+
+</div>
+
+ <center><h1>NBRP2P</h1></center>   
+ </header>  
+
+    <div style="margin: 5px;">
+
+ <div style="border:2px solid #E4E6EB;border-radius: 10px;font-family: \'Helvetica Neue\',Helvetica,Arial,sans-serif; max-width:100%; padding: 5px;">
+
+    <div style="float:left; width:50%; border: 0px solid #000; text-align:left; padding:5px 0px; clear:top;">    
+    
+    <span class="col-lg-3 col-md-4 col-xs-6 thumb"><img style="max-width: 50px;" src="https://md.br-com.net/img/NIOBIOCASH.png" /></span>
+
+  </div>  
+   
+  
+    <div style="float:right;  text-align:center;padding:5px 0px; width:50%; clear:top; font-size: 4.8vh">
+        
+   <b>Niobio Cash (NBR) </b>
+    <br />
+R$ 0,008 BRL 
+   <br />
+   
+    
+   </div>
+   
+    </div>
+    
+    <div style="border-top: 1px solid #E4E6EB;text-align:center; clear:both; font-size:10px; font-style:italic;padding:5px 0;">   
+    
+<a targe="_blank" href="https://md.br-com.net">md.br-com.net</a>
+ </div>
+ 
+ 
+ </div>
+
+
+
+<footer>
+   © Copyright - 2017 - <?php echo date(Y); ?> - Todos os Direitos Reservados
+
+
+</footer>
+</boby>
+
+</html>    
+
